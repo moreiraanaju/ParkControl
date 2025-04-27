e73aa12cd51d74ac6af70d6f9268d15c279a2806
moreiraanaju <123838863+moreiraanaju@users.noreply.github.com>
2025-04-27 16:56:58 -0300

    Criação das verificações dos campos do formulário de cadastro e configuração das mensagens de erro

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 
 const formulario = document.querySelector("form");
+const mensagem = document.getElementById("mensagem");
+
 
 formulario.addEventListener("submit", function(evento) {
     evento.preventDefault();
+
+    // Limpa mensagem anterior
+    mensagem.textContent = "";
+    mensagem.style.color = "";
+
+    // Checar se todos os campos obrigatórios foram preenchidos corretamente
+    if (!formulario.checkValidity()) {
+      mensagem.textContent = "Por favor, preencha todos os campos obrigatórios corretamente.";
+      mensagem.style.color = "red";
+      return;
+    }
+
+    //valida datas
+    const dataInicio = formulario.data_inicio.value;
+    const dataFim = formulario.data_fim.value;
+
+   
+    if (dataInicio > dataFim) {
+      mensagem.textContent = "Erro: A data de término não pode ser anterior à data de início.";
+      mensagem.style.color = "red";
+      return;
+    }
 
     const reserva = {
         nome: formulario.nome.value,
@@ -27,8 +51,8 @@ formulario.addEventListener("submit", function(evento) {
     localStorage.setItem("reservas", JSON.stringify(reservas));
 
     //mostra mensagem de sucesso
-    const mensagem = document.getElementById ("mensagem");
     mensagem.textContent = "Reserva efetuada com sucesso!";
+    mensagem.style.color = "green";
 
     //limpa o formulário
     formulario.reset();
